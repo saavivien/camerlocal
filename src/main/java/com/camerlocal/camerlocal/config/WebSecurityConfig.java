5555a6204dc28a3b366e6f7a5c819d5957c872d8
Unknown <unknown@example.com>
2019-10-31 08:43:22 +0100

    CORS integration

--- src/main/java/com/camerlocal/camerlocal/config/WebSecurityConfig.java
+++ src/main/java/com/camerlocal/camerlocal/config/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -65,14 +66,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/oauth/token").permitAll()
+                .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .httpBasic()
-                .realmName(securityRealm)
-                .and()
-                .csrf()
-                .disable();
+                .realmName(securityRealm);
+
     }
 
     @Bean
